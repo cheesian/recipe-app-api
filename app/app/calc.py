b399feec1418c747925657a7857fd3b1ea7e4e40
Ian M <ianmilimu90@gmail.com>
2024-12-10 12:37:07 +0300

    Added simple test cases

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+'''
+CALCULATOR FUNCTIONS
+'''
+
+def add(x, y):
+    '''
+    Adds two values and returns result
+    '''
+    return x + y
+
+def subtract(x, y):
+    '''
+    Adds two values and returns result
+    '''
+    return y - x
